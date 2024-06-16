e6716862a1dba535f78fdb155e1555647a4a5f5d
jbazann <joa7.baz@gmail.com>
2024-06-16 15:22:02 -0300

    add actuator debug utilities

--- config/src/main/java/com/jbazann/config/ConfigApplication.java
+++ config/src/main/java/com/jbazann/config/ConfigApplication.java
@@ -1,8 +1,11 @@
 package com.jbazann.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -10,6 +13,11 @@ public class ConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+	@Bean
+	public HttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
 	}
 
 }
