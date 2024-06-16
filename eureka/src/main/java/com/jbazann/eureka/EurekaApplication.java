cd0c8d1b02847c5a1a81929cd989d1d2424ce433
jbazann <joa7.baz@gmail.com>
2024-06-16 15:24:12 -0300

    add eureka maven module

---
+++ eureka/src/main/java/com/jbazann/eureka/EurekaApplication.java
@@ -0,0 +1,23 @@
+package com.jbazann.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+	@Bean
+	public HttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
+	}
+
+}
