9a01c143f27ed8a16be6a14f049f050d479e57ff
jbazann <joa7.baz@gmail.com>
2024-07-06 16:53:10 -0300

    remove unnecessary annotation

--- customers/src/main/java/com/jbazann/customers/persistence/repositories/SiteRepository.java
+++ customers/src/main/java/com/jbazann/customers/persistence/repositories/SiteRepository.java
@@ -2,10 +2,8 @@ package com.jbazann.customers.persistence.repositories;
 
 import com.jbazann.customers.domain.entities.Site;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@Repository
 public interface SiteRepository extends JpaRepository<Site, UUID> {
 }
