f6707af8ef1af7710c3567af92619172b4e1db45
jbazann <joa7.baz@gmail.com>
2024-12-05 19:27:41 -0300

    Slightly less half-assed integration tests.

--- orders/src/test/java/com/jbazann/orders/OrdersApplicationTests.java
+++ orders/src/test/java/com/jbazann/orders/OrdersApplicationTests.java
@@ -2,6 +2,7 @@ package com.jbazann.orders;
 
 import com.jbazann.orders.mocks.CustomersRemoteServiceMock;
 import com.jbazann.orders.mocks.ProductsRemoteServiceMock;
+import com.jbazann.orders.order.dto.NewOrderDTO;
 import com.jbazann.orders.order.dto.OrderDTO;
 import com.jbazann.orders.order.dto.StatusUpdateDTO;
 import com.jbazann.orders.order.entities.Order;
@@ -10,7 +11,6 @@ import com.jbazann.orders.order.services.CustomersRemoteServiceInterface;
 import com.jbazann.orders.order.OrderRepository;
 import com.jbazann.orders.order.services.ProductsRemoteServiceInterface;
 import com.jbazann.orders.testdata.StandardDataset;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +21,15 @@ import org.springframework.test.context.bean.override.convention.TestBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import static com.jbazann.orders.testdata.StandardDataset.*;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
@@ -35,8 +39,10 @@ class OrdersApplicationTests {
 	private final WebTestClient webClient;
 	private final OrderRepository orderRepository;
 
-	/**
-	 * This is only a field because @TestBean doesn't work on methods.
+	/*
+	 * These fields and static factory methods only exist because @TestBean
+	 * only works this way. It still appears to be the simplest solution
+	 * to replace Beans with mocks.
 	 */
 	@TestBean(enforceOverride = true)
 	private CustomersRemoteServiceInterface customersRemoteServiceMock;
@@ -67,9 +73,9 @@ class OrdersApplicationTests {
 
 	@Test
 	void getOrderById() {
-		Assertions.assertTrue(PERSISTED_LIST_FROM_CUSTOMER.hasOrder());
-		final UUID id = PERSISTED_LIST_FROM_CUSTOMER.orderId();
-		webClient.get().uri("/orders/{id}",id)
+		assertTrue(PERSISTED_LIST_FROM_CUSTOMER.hasOrder());
+		final UUID ORDER_ID = PERSISTED_LIST_FROM_CUSTOMER.orderId();
+		webClient.get().uri("/order/{id}",ORDER_ID)
 				.exchange()
 				.expectStatus().isOk()
 				.expectBody(OrderDTO.class)
@@ -78,19 +84,25 @@ class OrdersApplicationTests {
 
 	@Test
 	void getCustomerOrders() {
-		Assertions.assertTrue(PERSISTED_LIST_FROM_CUSTOMER.hasCustomer());
-		final UUID id = PERSISTED_LIST_FROM_CUSTOMER.customerId();
-		List<OrderDTO> expected = DATA.stream()
-				.filter(order -> order.customer().equals(id))
+		assertTrue(PERSISTED_LIST_FROM_CUSTOMER.hasCustomer());
+		final UUID CUSTOMER_ID = PERSISTED_LIST_FROM_CUSTOMER.customerId();
+		final OrderDTO NOT_EXPECTED = NOT_FOUND.asOrderDTO();
+		final List<OrderDTO> EXPECTED = DATA.stream()
+				.filter(order -> order.customer().equals(CUSTOMER_ID))
 				.map(Order::toDto)
 				.toList();
-		webClient.get().uri("/customer/{id}", id)
+		assertFalse(EXPECTED.isEmpty());
+		assertNotEquals(NOT_EXPECTED.customer(), CUSTOMER_ID);
+		assertFalse(EXPECTED.contains(NOT_EXPECTED));
+		webClient.get().uri("/customer/{id}", CUSTOMER_ID)
 				.accept(MediaType.APPLICATION_JSON)
 				.exchange()
 				.expectHeader().contentType(MediaType.APPLICATION_JSON)
 				.expectStatus().isOk()
 				.expectBodyList(OrderDTO.class)
-				.isEqualTo(expected);
+				.hasSize(EXPECTED.size())
+				.contains(EXPECTED.toArray(new OrderDTO[0]))
+				.doesNotContain(NOT_EXPECTED);
 	}
 
 	@Test
@@ -102,7 +114,7 @@ class OrdersApplicationTests {
 				EMPTY_DETAIL,
 				NO_USER
 		);
-		Assertions.assertTrue(cases.stream().allMatch(StandardDataset::hasOrder));
+		assertTrue(cases.stream().allMatch(StandardDataset::hasOrder));
 		cases.stream().map(StandardDataset::asNewOrderDTO).forEach(order -> {
 			webClient.post().uri("/order")
 					.contentType(MediaType.APPLICATION_JSON)
@@ -110,35 +122,51 @@ class OrdersApplicationTests {
 					.accept(MediaType.APPLICATION_JSON)
 					.exchange()
 					.expectStatus().is4xxClientError()
-					.expectBody().isEmpty();
+					.expectBody(String.class);
 		});
 	}
 
 	@Test
 	void newOrder() {
-		Assertions.assertTrue(NEW_VALID.hasOrder());
+		assertTrue(NEW_VALID.hasOrder());
+		final NewOrderDTO NEW_ORDER = NEW_VALID.asNewOrderDTO();
 		webClient.post().uri("/order")
-				.bodyValue(NEW_VALID.asNewOrderDTO())
+				.bodyValue(NEW_ORDER)
 				.exchange()
 				.expectStatus().isCreated()
 				.expectBody(OrderDTO.class)
 				.value(response -> {
 					assertNotNull(response.id());
-					assertEquals(response, NEW_VALID.asOrderDTO().id(response.id()));
+					assertEquals(NEW_ORDER.customer(), response.customer());
+					assertEquals(NEW_ORDER.site(), response.site());
+					assertEquals(NEW_ORDER.user(), response.user());
+					assertEquals(NEW_ORDER.note(), response.note());
+					NEW_ORDER.detail().forEach(detail -> {
+						assertTrue(response.detail().stream()
+								.anyMatch(d -> Objects.equals(d.product(), detail.product()) &&
+										Objects.equals(d.amount(), detail.amount())));
+					});
+					response.detail().forEach(detail -> {
+						assertNotNull(detail.id());
+						assertNotNull(detail.totalCost());
+						assertNotNull(detail.discount());
+						assertNotNull(detail.unitCost());
+					});
 				});
 	}
 
 	@Test
 	void updateInvalid() {
-		Assertions.assertTrue(NOT_FOUND.hasOrder());
-		StatusUpdateDTO update = new StatusUpdateDTO(StatusHistory.Status.DELIVERED, "Some valid detail.");
+		assertTrue(NOT_FOUND.hasOrder());
+		final StatusUpdateDTO UPDATE = new StatusUpdateDTO(StatusHistory.Status.DELIVERED, "Some valid detail.");
 		webClient.put().uri("/order/{id}", NOT_FOUND.orderId())
 				.accept(MediaType.APPLICATION_JSON)
 				.contentType(MediaType.APPLICATION_JSON)
-				.bodyValue(update)
+				.bodyValue(UPDATE)
 				.exchange()
 				.expectHeader().contentType(MediaType.APPLICATION_JSON)
-				.expectStatus().isNotFound();
+				.expectStatus().isNotFound()
+				.expectBody(String.class);
 	}
 
 }
