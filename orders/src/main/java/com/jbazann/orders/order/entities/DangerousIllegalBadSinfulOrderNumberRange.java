6d9cae5ae266716591ca645ec38baae31aa1eec9
jbazann <joa7.baz@gmail.com>
2024-12-06 12:50:18 -0300

    Technical loan.

---
+++ orders/src/main/java/com/jbazann/orders/order/entities/DangerousIllegalBadSinfulOrderNumberRange.java
@@ -0,0 +1,31 @@
+package com.jbazann.orders.order.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+/**
+ * As long as this entity exists in the code, this service cannot be
+ * replicated. It is intended to support the assignation of a range unique
+ * to a service replica, in which sequentially generated order numbers
+ * must be guaranteed to be unique.
+ * Dealing with this, either by actually solving the problem or by properly documenting
+ * it, sounds like a problem too big for a toy project in this state. I most
+ * likely won't ever return to fix this.
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Accessors(chain = true, fluent = true)
+@Document
+public class DangerousIllegalBadSinfulOrderNumberRange {
+
+    private long base;
+    private long max;
+
+}
