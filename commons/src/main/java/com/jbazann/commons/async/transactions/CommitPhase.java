431723a2d6d2a3e1a9acbbf3d52e544a5a1c764e
jbazann <joa7.baz@gmail.com>
2024-12-18 11:34:14 -0300

    Make TransactionPhase a Spring Component.

--- commons/src/main/java/com/jbazann/commons/async/transactions/CommitPhase.java
+++ commons/src/main/java/com/jbazann/commons/async/transactions/CommitPhase.java
@@ -1,8 +1,16 @@
 package com.jbazann.commons.async.transactions;
 
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
 /**
  * Marks COMMIT {@link TransactionPhase} components so they can
  * be found by {@link TransactionPhaseRegistrar}.
  */
+@Component
 public @interface CommitPhase {
+
+    @AliasFor(annotation= Component.class)
+    String value();
+
 }
--- commons/src/main/java/com/jbazann/commons/async/transactions/ReservePhase.java
+++ commons/src/main/java/com/jbazann/commons/async/transactions/ReservePhase.java
@@ -1,8 +1,16 @@
 package com.jbazann.commons.async.transactions;
 
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
 /**
  * Marks RESERVE {@link TransactionPhase} components so they can
  * be found by {@link TransactionPhaseRegistrar}.
  */
+@Component
 public @interface ReservePhase {
+
+    @AliasFor(annotation=Component.class)
+    String value();
+
 }
