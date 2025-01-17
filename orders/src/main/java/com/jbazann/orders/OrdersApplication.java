ad6cdb91da49703337bb16038b65816a85019b9a
jbazann <joa7.baz@gmail.com>
2025-01-17 10:44:50 -0300

    Update commons.async.api annotations.

--- orders/src/main/java/com/jbazann/orders/OrdersApplication.java
+++ orders/src/main/java/com/jbazann/orders/OrdersApplication.java
@@ -1,18 +1,23 @@
 package com.jbazann.orders;
 
-import com.jbazann.commons.async.orchestration.OrchestrationCoordinator;
-import com.jbazann.commons.async.orchestration.OrchestrationMember;
+import com.jbazann.commons.async.api.DistributedTransactionCoordinator;
+import com.jbazann.commons.async.api.DistributedTransactionMember;
+import com.jbazann.commons.async.api.DistributedTransactionStarter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication()
 @EnableMongoRepositories("com.jbazann.orders.order")
-@OrchestrationMember
-@OrchestrationCoordinator
+@EnableTransactionManagement
+@DistributedTransactionCoordinator
+@DistributedTransactionMember
+@DistributedTransactionStarter
+//TODO review local transactions, locking
 public class OrdersApplication {
 
 	public static void main(String[] args) {
