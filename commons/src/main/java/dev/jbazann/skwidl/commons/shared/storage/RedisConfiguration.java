2753cf6d0441a5a04fe0403875a9c1d62224b090
jbazann <joa7.baz@gmail.com>
2025-01-21 12:52:34 -0300

    Replace Lettuce client with Redisson.

--- commons/src/main/java/dev/jbazann/skwidl/commons/shared/storage/RedisConfiguration.java
+++ commons/src/main/java/dev/jbazann/skwidl/commons/shared/storage/RedisConfiguration.java
@@ -1,14 +1,16 @@
 package dev.jbazann.skwidl.commons.shared.storage;
 
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.redisson.Redisson;
+import org.redisson.api.*;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+
 
 @Configuration
 @PropertySource("classpath:commons.properties")
@@ -17,10 +19,11 @@ public class RedisConfiguration {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(RedisConfigurationProperties properties) {
-        System.out.printf("### SKWIDL ### REDIS HOST: %s ### REDIS PORT: %d ###\n", properties.getHost(), properties.getPort());
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(
-                properties.getHost(), properties.getPort());
-        return factory;
+        final String RedisAddress = String.format("redis://%s:%d", properties.getHost(), properties.getPort());
+        Config config = new Config();
+        config.useSingleServer().setAddress(RedisAddress);
+        RedissonClient client = Redisson.create(config);
+        return  new RedissonConnectionFactory(client);
     }
 
     @Bean
