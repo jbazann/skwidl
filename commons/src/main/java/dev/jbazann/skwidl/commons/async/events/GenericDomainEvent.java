0efe648fc539832f7244e0fd5eadf5fba16244cf
jbazann <joa7.baz@gmail.com>
2025-02-09 11:11:24 -0300

    Fix copy() implementation to match spec.

--- commons/src/main/java/dev/jbazann/skwidl/commons/async/events/GenericDomainEvent.java
+++ commons/src/main/java/dev/jbazann/skwidl/commons/async/events/GenericDomainEvent.java
@@ -3,6 +3,6 @@ package dev.jbazann.skwidl.commons.async.events;
 public class GenericDomainEvent extends DomainEvent {
     @Override
     protected DomainEvent copy() {
-        return this;
+        return new GenericDomainEvent();
     }
 }
