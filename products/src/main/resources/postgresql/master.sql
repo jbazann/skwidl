f00e0e405d14efa368f9c21d1873734188fab252
jbazann <joa7.baz@gmail.com>
2025-02-19 14:00:36 -0300

    Fix schema privileges.

--- products/src/main/resources/postgresql/master.sql
+++ products/src/main/resources/postgresql/master.sql
@@ -5,6 +5,7 @@ CREATE SCHEMA product;
 
 CREATE USER product PASSWORD 'product';
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA product TO product;
+GRANT USAGE ON SCHEMA product TO product;
 
 CREATE DATABASE category;
 \c category;
@@ -13,3 +14,4 @@ CREATE SCHEMA category;
 
 CREATE USER category PASSWORD 'category';
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA category TO category;
+GRANT USAGE ON SCHEMA category TO category;
