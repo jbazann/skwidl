c736e5d03e7b9272e312a5836cdc8a620990972a
jbazann <joa7.baz@gmail.com>
2025-03-04 15:35:27 -0300

    Update generated schema.

--- customers/src/main/resources/mysql/site/ddl.sql
+++ customers/src/main/resources/mysql/site/ddl.sql
@@ -2,9 +2,9 @@
     create table site (
         id binary(16) not null,
         address varchar(255) not null,
-        budget decimal(38,2) not null,
-        coordinates varchar(255) not null,
+        budget decimal(38,2),
+        coordinates varchar(255),
         customer binary(16) not null,
-        status enum ('ACTIVE','PENDING','FINISHED') not null,
+        status enum ('UNSET','ACTIVE','PENDING','FINISHED') not null,
         primary key (id)
     ) engine=InnoDB;
--- customers/src/main/resources/mysql/user/ddl.sql
+++ customers/src/main/resources/mysql/user/ddl.sql
@@ -1,10 +1,19 @@
 
     create table user (
         id binary(16) not null,
-        customer binary(16) not null,
         dni varchar(255) not null,
         email varchar(255) not null,
         lastname varchar(255) not null,
         name varchar(255) not null,
         primary key (id)
     ) engine=InnoDB;
+
+    create table User_customers (
+        User_id binary(16) not null,
+        customers binary(16)
+    ) engine=InnoDB;
+
+    alter table User_customers 
+       add constraint FKhbn77ey2rty89p2xxthieegtf 
+       foreign key (User_id) 
+       references user (id);
