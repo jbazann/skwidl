6dad11ad1add1df65bf68269f99b02fdf91358c9
jbazann <joa7.baz@gmail.com>
2025-03-08 11:33:28 -0300

    Remove unused imports.

--- orders/src/main/java/dev/jbazann/skwidl/orders/order/services/CustomerServiceRestClient.java
+++ orders/src/main/java/dev/jbazann/skwidl/orders/order/services/CustomerServiceRestClient.java
@@ -5,7 +5,6 @@ import dev.jbazann.skwidl.commons.exceptions.UnexpectedResponseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
