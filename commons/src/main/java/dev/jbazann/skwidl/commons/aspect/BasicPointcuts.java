c136f3e1192f669b4f2da8824d249b15d91d10ea
jbazann <joa7.baz@gmail.com>
2025-03-08 13:48:31 -0300

    Log network requests.

--- commons/src/main/java/dev/jbazann/skwidl/commons/aspect/BasicPointcuts.java
+++ commons/src/main/java/dev/jbazann/skwidl/commons/aspect/BasicPointcuts.java
@@ -2,6 +2,7 @@ package dev.jbazann.skwidl.commons.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.web.client.RestClient;
 
 @Aspect
 public class BasicPointcuts {
@@ -11,6 +12,9 @@ public class BasicPointcuts {
 
     @Pointcut("anyMethod() && inProjectPackages() && inAnnotatedController()")
     public void controllerExecutions() {}
+
+    @Pointcut(value = "restClientExchange(handler) && inProjectPackages()", argNames = "handler")
+    public void restClientRequest(RestClient.RequestHeadersSpec.ExchangeFunction<?> handler) {}
 
     @Pointcut("execution(* *(..))")
     private void anyMethod() {}
@@ -23,5 +27,8 @@ public class BasicPointcuts {
 
     @Pointcut("@target(org.springframework.stereotype.Controller)")
     private void inAnnotatedController() {}
+
+    @Pointcut("execution(* org.springframework.web.client.RestClient.RequestBodySpec.exchange(..)) && args(handler)")
+    private void restClientExchange(RestClient.RequestHeadersSpec.ExchangeFunction<?> handler) {}
 
 }
