da40f1b16ec66260e656559fe177863e597c41d9
jbazann <joa7.baz@gmail.com>
2025-03-14 12:25:50 -0300

    Set prototype scope for RestClient builder.

--- commons/src/main/java/dev/jbazann/skwidl/commons/rest/RestClientConfiguration.java
+++ commons/src/main/java/dev/jbazann/skwidl/commons/rest/RestClientConfiguration.java
@@ -1,13 +1,16 @@
 package dev.jbazann.skwidl.commons.rest;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.autoconfigure.web.client.RestClientBuilderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfiguration {
     
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     @Bean("loggingRestClientBuilder")
     public RestClient.Builder loggingRestClientBuilder(RestClientBuilderConfigurer c) {
         RestClient.Builder clientBuilder = RestClient.builder();
