a3b9b44c52132351c51ada61782c0ba68ffcd93d
jbazann <joa7.baz@gmail.com>
2025-06-11 14:45:16 -0300

    Better (?) logging utilities.

--- commons/src/main/java/dev/jbazann/skwidl/commons/logging/Logger.java
+++ commons/src/main/java/dev/jbazann/skwidl/commons/logging/Logger.java
@@ -1,22 +1,20 @@
 package dev.jbazann.skwidl.commons.logging;
 
-import java.lang.reflect.Method;
-
 public class Logger {
 
     private final org.slf4j.Logger logger;
+    private static final StackWalker stackWalker = StackWalker.getInstance();
 
     protected Logger(org.slf4j.Logger logger) {
         this.logger = logger;
     }
 
+    /**
+     * @deprecated
+     */
     public void method(StackTraceElement[] stack, Object... params) {
-        StringBuilder p = new StringBuilder("[");
-        for (Object param : params) {
-            p.append(param).append(";");
-        }
-        if (p.charAt(p.length() - 1) == ';') p.deleteCharAt(p.length() - 1);
-        p.append("]");
+        if (!logger.isDebugEnabled()) return;
+        String p = paramsString(params);
         logger.debug(String.format(
                 "Call %s with params %s",
                 stack.length == 0 ? "UNKNOWN" : stack[0].getMethodName(),
@@ -24,22 +22,142 @@ public class Logger {
         ));
     }
 
-    public void result(Object val) {
+    public void method(Object... params) {
+        if (!logger.isDebugEnabled()) return;
+        String p = paramsString(params);
+        String caller = stackWalker.walk(s -> s
+                .skip(1)
+                .findFirst()
+                .map(StackWalker.StackFrame::getMethodName)
+                .orElse("UNKNOWN"));
         logger.debug(String.format(
-                "Return %s",
-                val
+                "Call %s with params %s",
+                caller,
+                p
         ));
     }
 
-    public void result(Object val, String identifier) {
+    /**
+     * Debug logging utility intended to provide more
+     * granular observability than what is already covered
+     * by AOP logging.
+     * @param val the calling method's return value.
+     * @return the logged value.
+     * @apiNote for transparency, this method's return value is
+     * to be ignored, unless doing so requires the creation of a
+     * variable, or writing more additional lines than the expected
+     * return statement.
+     * For example, to log the result of:
+     * <pre>{@code
+     *     O m(P p) {
+     *         ...
+     *         return foo(p);
+     *     }
+     * }</pre>
+     * The preferred form is:
+     * <pre>{@code
+     *     O m(P p) {
+     *         ...
+     *         return log.result(foo(p));
+     *     }
+     * }</pre>
+     * Over:
+     * <pre>{@code
+     *     O m(P p) {
+     *         ...
+     *         O r = foo(p);
+     *         log.result(r);
+     *         return r;
+     *     }
+     * }</pre>
+     * This is also a valid pattern with fluent APIs or other inline
+     * scenarios that simply wrap an unreferenced value.
+     * But if the reference is needed for more than this
+     * intermediate call, the last form is preferred.
+     */
+    public <T> T result(T val) {
+        if (!logger.isDebugEnabled()) return val;
+        String caller = stackWalker.walk(s -> s
+                .skip(1)
+                .findFirst()
+                .map(StackWalker.StackFrame::getMethodName)
+                .orElse("UNKNOWN"));
         logger.debug(String.format(
-                "Return %s at %s",
-                val
+                "Return %s from %s",
+                val,
+                caller
         ));
+        return val;
+    }
+
+    /**
+     * Debug logging utility intended to provide more
+     * granular observability than what is already covered
+     * by AOP logging.
+     * @param val the calling method's return value.
+     * @param identifier a return point identifier, for
+     *                   methods with multiple return statements.
+     * @return the logged value.
+     * @apiNote for transparency, this method's return value is
+     * to be ignored, unless doing so requires the creation of a
+     * variable, or writing more additional lines than the expected
+     * return statement.
+     * For example, to log the result of:
+     * <pre>{@code
+     *     O m(P p) {
+     *         ...
+     *         return foo(p);
+     *     }
+     * }</pre>
+     * The preferred form is:
+     * <pre>{@code
+     *     O m(P p) {
+     *         ...
+     *         return log.result(foo(p));
+     *     }
+     * }</pre>
+     * Over:
+     * <pre>{@code
+     *     O m(P p) {
+     *         ...
+     *         O r = foo(p);
+     *         log.result(r);
+     *         return r;
+     *     }
+     * }</pre>
+     * This is also a valid pattern with fluent APIs or other inline
+     * scenarios that simply wrap an unreferenced value.
+     * But if the reference is needed for more than this
+     * intermediate call, the last form is preferred.
+     */
+    public <T> T result(T val, String identifier) {
+        if (!logger.isDebugEnabled()) return val;
+        String caller = stackWalker.walk(s -> s
+                .skip(1)
+                .findFirst()
+                .map(StackWalker.StackFrame::getMethodName)
+                .orElse("UNKNOWN"));
+        logger.debug(String.format(
+                "Return %s from %s at %s.",
+                val,
+                caller,
+                identifier
+        ));
+        return val;
     }
 
     public void debug(String format, Object... params) {
         logger.debug(format,params);
+    }
+
+    private String paramsString(Object... params) {
+        StringBuilder p = new StringBuilder("[");
+        for (Object param : params) {
+            p.append(param).append(";");
+        }
+        if (p.charAt(p.length() - 1) == ';') p.deleteCharAt(p.length() - 1);
+        p.append("]");
+        return p.toString();
     }
 
 }
