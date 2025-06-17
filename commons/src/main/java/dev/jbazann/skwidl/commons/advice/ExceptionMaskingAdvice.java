4ef0acf8c580adf1504ee367d88e94a5c9ef5b05
jbazann <joa7.baz@gmail.com>
2025-06-17 20:18:49 -0300

    Custom masking handler for ConstraintViolationException.

--- commons/src/main/java/dev/jbazann/skwidl/commons/advice/ExceptionMaskingAdvice.java
+++ commons/src/main/java/dev/jbazann/skwidl/commons/advice/ExceptionMaskingAdvice.java
@@ -1,11 +1,14 @@
 package dev.jbazann.skwidl.commons.advice;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,6 +26,38 @@ public class ExceptionMaskingAdvice {
         String message = String.format(
                 "CAUGHT: Uncaught exception of type %s, ID: %s, message: %s",
                 e.getClass().getName(), id, e.getMessage());
+        // TODO use the id and the stack trace to make the logs actually useful
+        logger.log(Level.WARNING, message, e);
+        return "An unexpected error occurred. Incident ID: " + id;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleConstraintViolationException(ConstraintViolationException e) {
+        UUID id = UUID.randomUUID();
+        StringBuilder violations = new StringBuilder("[\n");
+        for (ConstraintViolation<?> violation : e.getConstraintViolations()) {
+            violations.append(violation.getPropertyPath())
+                    .append(": ")
+                    .append(violation.getInvalidValue())
+                    .append("\nRETURN - ")
+                    .append(violation.getExecutableReturnValue())
+                    .append("\nPARAMETERS - ")
+                    .append(Arrays.toString(violation.getExecutableParameters()))
+                    .append("\nLEAF BEAN - ")
+                    .append(violation.getLeafBean())
+                    .append("\nROOT BEAN - ")
+                    .append(violation.getRootBean())
+                    .append("\nCONSTRAINT DESCRIPTOR - ")
+                    .append(violation.getConstraintDescriptor())
+                    .append("\n");
+        }
+        violations.append("]");
+        String message = String.format(
+                "CAUGHT: Uncaught exception of type %s, ID: %s, message: %s. \nCONSTRAINT VIOLATIONS: %s",
+                e.getClass().getName(), id, e.getMessage(),
+                violations
+                );
         // TODO use the id and the stack trace to make the logs actually useful
         logger.log(Level.WARNING, message, e);
         return "An unexpected error occurred. Incident ID: " + id;
