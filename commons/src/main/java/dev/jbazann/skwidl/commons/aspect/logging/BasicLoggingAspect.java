7cbe7ac00604dd6c9e6e6d7967c1ceb34db240c1
jbazann <joa7.baz@gmail.com>
2025-09-10 21:32:05 -0300

    Log Repository return values.

--- commons/src/main/java/dev/jbazann/skwidl/commons/aspect/logging/BasicLoggingAspect.java
+++ commons/src/main/java/dev/jbazann/skwidl/commons/aspect/logging/BasicLoggingAspect.java
@@ -1,6 +1,8 @@
 package dev.jbazann.skwidl.commons.aspect.logging;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -17,14 +19,26 @@ public class BasicLoggingAspect {
     }
 
     @Before("dev.jbazann.skwidl.commons.aspect.BasicPointcuts.serviceExecutions() || " +
-            "dev.jbazann.skwidl.commons.aspect.BasicPointcuts.controllerExecutions() || " +
-            "dev.jbazann.skwidl.commons.aspect.BasicPointcuts.repositoryExecutions()")
+            "dev.jbazann.skwidl.commons.aspect.BasicPointcuts.controllerExecutions()")
     public void logMethodExecutions(JoinPoint joinPoint) {
         logger.info("Invoking " + joinPoint.getSignature().toShortString() +
                         (joinPoint.getArgs().length > 0 ?
-                                " with argument(s) " + Arrays.toString(joinPoint.getArgs()) :
+                                " with argument(s):\n" + Arrays.toString(joinPoint.getArgs()) :
                                 "")
         );
+    }
+
+    @Around("dev.jbazann.skwidl.commons.aspect.BasicPointcuts.repositoryExecutions()")
+    public Object logRepositoryExecutions(ProceedingJoinPoint pjp) 
+        throws Throwable {
+        logger.info("Invoking " + pjp.getSignature().toShortString() +
+                    (pjp.getArgs().length > 0 ?
+                            " with argument(s):\n" + Arrays.toString(pjp.getArgs()) :
+                            "")
+        );
+        Object result = pjp.proceed(pjp.getArgs());
+        logger.info("Repository call returned:\n" + result);
+        return result;
     }
 
 }
